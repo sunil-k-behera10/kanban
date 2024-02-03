6e1303a10f530011c27c22051a3f60f2062183ff
sunil-k-behera10 <sunil01090@gmail.com>
2024-02-03 14:57:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [list,setList] = useState(['Apple', 'Banana','Orange']);
+
+  const [dropin, setDropin] = useState(null);
+  const ondragstart = (e) => {
+    e.target.classList.add("dragfruits");
+  };
+  const ondragend = (e) => {
+    e.target.classList.remove("dragfruits");
+    const new_list = [...list];
+    new_list.splice(e.target.id, 1);
+    new_list.splice(dropin, 0, e.target.innerText);
+    setList(new_list);
+  };
+  const ondragover = (e) => {
+    e.preventDefault();
+    setDropin(e.target.id);
+  };
+
+  return(
+  <div className='app'>
+    <h2>Fruits</h2>
+      <div className='input-group'>
+        <input type="text" name="" id="" placeholder='fruits'/>
+        <button className='btn'>Click me</button>
+      </div>
+
+
+      <div className='list-container'>
+        {
+          list.map((v,i)=>(
+            <div key ={i} id={i} className='listitem' 
+            draggable onDragStart={ondragstart} 
+             onDragEnd={ondragend}
+             onDragOver={ondragover}
+             
+             >
+              {v}
+              
+            </div>
+
+          ))
+        }
+        
+        
+      </div>
+  </div>
   );
 }
 
